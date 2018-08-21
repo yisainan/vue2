c93e794dfa6739ae0f74c1f4aa2380e7dac3d507
齐磊 <qiilee@126.com>
2018-08-21 17:33:33 +0800

    Del：删除网易云及进度组件

--- progress/build/gh-pages.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-npm run demo:prepublish
-cd gh-pages
-git init
-git add -A
-git commit -m 'update gh-pages'
-git push -f git@github.com:vue-multiple/progress.git master:gh-pages
